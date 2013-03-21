1d1301c24f0f346c5552ddc48052bb95925bbc36
Aapeli Vuorinen <aapelisuomi@hotmail.com>
2013-03-21 17:39:38 +1000

    Added GPLv3

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <html>
 	<?php
+	// © 2012 - 2013 Aapeli Vuorinen
+	// visit my website at http://aapeliv.com/ (if it is still up!)
+	
+	// This program is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+
+    // This program is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+
+    // You should have received a copy of the GNU General Public License
+    // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
 	session_start();
 	// VARIABLES
 	// $_SESSION['stage'];
@@ -284,7 +300,7 @@
 		<a href="?newgame">New game</a>
 		<center>
 			<p class="copy_title">&#169; 2012 - 2013 Aapeli&#8482;</p>
-			<p class="copy_body">Insert licence here</p>
+			<p class="copy_body">This program is free software: you can redistribute it and/or modify<br />it under the terms of the GNU General Public License as published by<br />the Free Software Foundation, either version 3 of the License, or<br />(at your option) any later version.<br /><br />This program is distributed in the hope that it will be useful,<br />but WITHOUT ANY WARRANTY; without even the implied warranty of<br />    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br />GNU General Public License for more details.<br /><br />You should have received a copy of the GNU General Public License<br />along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</p>
 		</center>
 	</body>
 </html>
